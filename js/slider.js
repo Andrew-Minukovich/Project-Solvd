5e265078fb5a7d11189a3ac832117d39dbcaf38a
WerJJJ <marikariwest@gmail.com>
2021-06-25 12:25:59 +0300

    add 50%

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,52 @@
+function countdown(dateEnd) {
+    let timer, days, hours, minutes, seconds;
+
+    dateEnd = new Date(dateEnd);
+    dateEnd = dateEnd.getTime();
+
+    if (isNaN(dateEnd)) {
+        return;
+    }
+
+    timer = setInterval(calculate, 1000);
+
+    function calculate() {
+        let dateStart = new Date();
+        dateStart = new Date(dateStart.getUTCFullYear(),
+            dateStart.getUTCMonth(),
+            dateStart.getUTCDate(),
+            dateStart.getUTCHours(),
+            dateStart.getUTCMinutes(),
+            dateStart.getUTCSeconds());
+        let timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
+
+        if (timeRemaining >= 0) {
+            days = parseInt(timeRemaining / 86400);
+            timeRemaining = (timeRemaining % 86400);
+            hours = parseInt(timeRemaining / 3600);
+            timeRemaining = (timeRemaining % 3600);
+            minutes = parseInt(timeRemaining / 60);
+            timeRemaining = (timeRemaining % 60);
+            seconds = parseInt(timeRemaining);
+
+
+            document.getElementById("days").innerHTML = parseInt(days, 10);
+            document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+            document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+            document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+        } else {
+            return;
+        }
+    }
+
+    function display(days, hours, minutes, seconds) { }
+}
+
+
+
+
+countdown('09/22/2022 02:00:00 AM');
+
 new Swiper('.our-partners__slider', {
     loop: true,
     slidesPerView: 7,
@@ -13,15 +62,15 @@ new Swiper('.our-partners__slider', {
         el: ".swiper-pagination",
         clickable: true,
     }
-  });
+});
 
-  new Swiper('.road-map-content__progress-bar', {
+new Swiper('.road-map-content__progress-bar', {
     slidesPerView: 5,
     grabCursor: true,
     loop: false,
     direction: 'horizontal',
     watchSlidesVisibility: true,
-    
+
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -29,6 +78,53 @@ new Swiper('.our-partners__slider', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      }
-  });
+    }
+});
 
+new Swiper('.team-content', {
+
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 4,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+new Swiper('.news__content', {
+
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+new Swiper('.in-press__content', {
+
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
