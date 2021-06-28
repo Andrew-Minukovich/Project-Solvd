98946fd84f2052f204edb5f0c1b09521c70f05b5
WerJJJ <marikariwest@gmail.com>
2021-06-29 01:34:48 +0300

    feat: add 50% animations

--- js/scroll.js
+++ js/scroll.js
@@ -7,7 +7,7 @@ gsap.from(".main-header__header-wrapper", {
     duration: 1,
     opacity: 0,
     translateX: 200,
-    transformOrigin: "center center",  
+    transformOrigin: "center center",
     ease: "back"
 });
 
@@ -20,34 +20,34 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".intro-header__content-right", {
-    duration: 1,
-    opacity: 0,
-    translateX: 100,
-    stagger: 0.2,
-    ease: "back"
-})
-.from(".intro-header__title", {
-    duration: 0.4,
-    opacity: 0,
-    translateX: -100,
-    stagger: 0.2,
-    ease: "back"
-})
-.from(".intro-header__description", {
-    duration: 0.4,
-    opacity: 0,
-    translateX: -100,
-    stagger: 0.2,
-    ease: "back"
-})
-.from(".intro-header__button", {
-    duration: 0.4,
-    opacity: 0,
-    translateX: -100,
-    stagger: 0.2,
-    ease: "back"
-});
+    .from(".intro-header__content-right", {
+        duration: 1,
+        opacity: 0,
+        translateX: 100,
+        stagger: 0.2,
+        ease: "back"
+    })
+    .from(".intro-header__title", {
+        duration: 0.4,
+        opacity: 0,
+        translateX: -100,
+        stagger: 0.2,
+        ease: "back"
+    })
+    .from(".intro-header__description", {
+        duration: 0.4,
+        opacity: 0,
+        translateX: -100,
+        stagger: 0.2,
+        ease: "back"
+    })
+    .from(".intro-header__button", {
+        duration: 0.4,
+        opacity: 0,
+        translateX: -100,
+        stagger: 0.2,
+        ease: "back"
+    });
 
 gsap.timeline({
     scrollTrigger: {
@@ -56,26 +56,26 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".intro-content__progress-bar", {
-    duration: 1,
-    opacity: 0,
-    translateX: -100,
-    ease: "back"
-})
-.from(".intro-content__point", {
-    duration: 0.9,
-    opacity: 0,
-    translateX: -100,
-    stagger: 0.5,
-    ease: "back"
-})
-.from(".intro-content__target-item", {
-    duration: 0.9,
-    opacity: 0,
-    translateX: -100,
-    stagger: 0.5,
-    ease: "back"
-});
+    .from(".intro-content__progress-bar", {
+        duration: 1,
+        opacity: 0,
+        translateX: -100,
+        ease: "back"
+    })
+    .from(".intro-content__point", {
+        duration: 0.9,
+        opacity: 0,
+        translateX: -100,
+        stagger: 0.5,
+        ease: "back"
+    })
+    .from(".intro-content__target-item", {
+        duration: 0.9,
+        opacity: 0,
+        translateX: -100,
+        stagger: 0.5,
+        ease: "back"
+    });
 
 gsap.timeline({
     scrollTrigger: {
@@ -84,37 +84,37 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".intro-content__bonus-title", {
-    duration: 1,
-    opacity: 0,
-    translateX: 100,
-    ease: "back"
-})
-.from(".intro-content__bonus-value", {
-    duration: 0.4,
-    opacity: 0,
-    translateX: 100,
-    ease: "back"
-})
-.from(".intro-content__bonus-ownership", {
-    duration: 0.4,
-    opacity: 0,
-    translateX: 100,
-    ease: "back"
-})
-.from(".intro-content__bonus-end-title", {
-    duration: 0.4,
-    opacity: 0,
-    translateX: 100,
-    ease: "back"
-})
-.from(".time-section", {
-    duration: 0.4,
-    opacity: 0,
-    translateX: 100,
-    stagger: 0.4,
-    ease: "back"
-});
+    .from(".intro-content__bonus-title", {
+        duration: 1,
+        opacity: 0,
+        translateX: 100,
+        ease: "back"
+    })
+    .from(".intro-content__bonus-value", {
+        duration: 0.4,
+        opacity: 0,
+        translateX: 100,
+        ease: "back"
+    })
+    .from(".intro-content__bonus-ownership", {
+        duration: 0.4,
+        opacity: 0,
+        translateX: 100,
+        ease: "back"
+    })
+    .from(".intro-content__bonus-end-title", {
+        duration: 0.4,
+        opacity: 0,
+        translateX: 100,
+        ease: "back"
+    })
+    .from(".time-section", {
+        duration: 0.4,
+        opacity: 0,
+        translateX: 100,
+        stagger: 0.4,
+        ease: "back"
+    });
 
 
 gsap.timeline({
@@ -124,20 +124,20 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".intro-content__item", {
-    duration: 1,
-    opacity: 0,
-    translateX: -100,
-    stagger: 0.4,
-    ease: "back"
-})
-.from(".social-media__element", {
-    duration: 0.4,
-    opacity: 0,
-    translateX: 100,
-    stagger: 0.4,
-    ease: "back"
-});
+    .from(".intro-content__item", {
+        duration: 1,
+        opacity: 0,
+        translateX: -100,
+        stagger: 0.4,
+        ease: "back"
+    })
+    .from(".social-media__element _intro", {
+        duration: 0.4,
+        opacity: 0,
+        translateX: 100,
+        stagger: 0.4,
+        ease: "back"
+    });
 
 //ecosystems
 
@@ -162,13 +162,13 @@ gsap.timeline({
     },
     delay: 0.6
 })
-.from(".ecosystems__advantage", {
-    duration: 1,
-    opacity: -0.5,
-    translateY: 40,
-    stagger: 0.1,
-    ease: "back"
-});
+    .from(".ecosystems__advantage", {
+        duration: 1,
+        opacity: -0.5,
+        translateY: 40,
+        stagger: 0.1,
+        ease: "back"
+    });
 
 //platform
 
@@ -204,18 +204,18 @@ gsap.timeline({
     },
     delay: 0.1
 })
-.from(".platform__list-title", {
-    duration: 0.6,
-    opacity: 0,
-    scale: 0,
-    ease: "back"
-})
-.from(".platform__list-subtitle", {
-    duration: 0.6,
-    opacity: 0,
-    scale: 0,
-    ease: "back"
-});
+    .from(".platform__list-title", {
+        duration: 0.6,
+        opacity: 0,
+        scale: 0,
+        ease: "back"
+    })
+    .from(".platform__list-subtitle", {
+        duration: 0.6,
+        opacity: 0,
+        scale: 0,
+        ease: "back"
+    });
 
 gsap.timeline({
     scrollTrigger: {
@@ -224,20 +224,20 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".mvp-list__item", {
-    duration: 0.6,
-    opacity: 0,
-    stagger: 0.3,
-    translateY: 40,
-    ease: "back"
-})
-.from(".platform__wrapper-rounds", {
-    duration: 0.6,
-    opacity: 0,
-    stagger: 0.1,
-    translateY: 40,
-    ease: "back"
-});
+    .from(".mvp-list__item", {
+        duration: 0.6,
+        opacity: 0,
+        stagger: 0.3,
+        translateY: 40,
+        ease: "back"
+    })
+    .from(".platform__wrapper-rounds", {
+        duration: 0.6,
+        opacity: 0,
+        stagger: 0.1,
+        translateY: 40,
+        ease: "back"
+    });
 
 //Mvp
 
@@ -248,18 +248,18 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".mvp__header", {
-    duration: 1,
-    opacity: 0,
-    translateY: 40,
-    ease: "back"
-})
-.from(".mvp__description", {
-    duration: 0.6,
-    opacity: 0,
-    translateY: 40,
-    ease: "back"
-});
+    .from(".mvp__header", {
+        duration: 1,
+        opacity: 0,
+        translateY: 40,
+        ease: "back"
+    })
+    .from(".mvp__description", {
+        duration: 0.6,
+        opacity: 0,
+        translateY: 40,
+        ease: "back"
+    });
 
 gsap.timeline({
     scrollTrigger: {
@@ -268,27 +268,27 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".mvp__item", {
-    duration: 0.6,
-    opacity: 0,
-    stagger: 0.3,
-    translateY: 40,
-    ease: "back"
-})
-.from(".mvp__button", {
-    duration: 0.6,
-    opacity: 0,
-    stagger: 0.1,
-    translateY: 40,
-    ease: "back"
-})
-.from(".mvp__content-left", {
-    duration: 0.6,
-    opacity: 0,
-    stagger: 0.1,
-    translateY: 40,
-    ease: "back"
-});
+    .from(".mvp__item", {
+        duration: 0.6,
+        opacity: 0,
+        stagger: 0.3,
+        translateY: 40,
+        ease: "back"
+    })
+    .from(".mvp__button", {
+        duration: 0.6,
+        opacity: 0,
+        stagger: 0.1,
+        translateY: 40,
+        ease: "back"
+    })
+    .from(".mvp__content-left", {
+        duration: 0.6,
+        opacity: 0,
+        stagger: 0.1,
+        translateY: 40,
+        ease: "back"
+    });
 
 //tokens
 
@@ -299,18 +299,18 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".tokens__header", {
-    duration: 1,
-    opacity: 0,
-    translateY: 40,
-    ease: "back"
-})
-.from(".tokens_breakdown", {
-    duration: 0.6,
-    opacity: 0,
-    translateY: 40,
-    ease: "back"
-});
+    .from(".tokens__header", {
+        duration: 1,
+        opacity: 0,
+        translateY: 40,
+        ease: "back"
+    })
+    .from(".tokens_breakdown", {
+        duration: 0.6,
+        opacity: 0,
+        translateY: 40,
+        ease: "back"
+    });
 
 gsap.timeline({
     scrollTrigger: {
@@ -319,13 +319,13 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".tokens__tokens-card", {
-    duration: 1,
-    stagger: 0.5,
-    opacity: 0,
-    translateX: -140,
-    ease: "back"
-});
+    .from(".tokens__tokens-card", {
+        duration: 1,
+        stagger: 0.5,
+        opacity: 0,
+        translateX: -140,
+        ease: "back"
+    });
 
 gsap.timeline({
     scrollTrigger: {
@@ -334,13 +334,13 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".tokens-type", {
-    duration: 1,
-    stagger: 0.2,
-    opacity: 0,
-    translateX: 140,
-    ease: "back"
-});
+    .from(".tokens-type", {
+        duration: 1,
+        stagger: 0.2,
+        opacity: 0,
+        translateX: 140,
+        ease: "back"
+    });
 
 //roadmap
 
@@ -351,18 +351,18 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".road-map__header", {
-    duration: 1,
-    opacity: 0,
-    translateY: 40,
-    ease: "back"
-})
-.from(".road-map-header__description", {
-    duration: 0.6,
-    opacity: 0,
-    translateY: 40,
-    ease: "back"
-});
+    .from(".road-map__header", {
+        duration: 1,
+        opacity: 0,
+        translateY: 40,
+        ease: "back"
+    })
+    .from(".road-map-header__description", {
+        duration: 0.6,
+        opacity: 0,
+        translateY: 40,
+        ease: "back"
+    });
 
 gsap.timeline({
     scrollTrigger: {
@@ -371,17 +371,409 @@ gsap.timeline({
         markers: true
     }
 })
-.from(".road-map-content__item", {
-    duration: 1,
-    opacity: 0,
-    translateX: -100,
-    stagger: 0.2,
-    ease: "power2"
+    .from(".road-map-content__item", {
+        duration: 1,
+        opacity: 0,
+        translateX: -100,
+        stagger: 0.2,
+        ease: "power2"
+    })
+    .from(".road-map-content__btn__buttons", {
+        duration: 1,
+        opacity: 0,
+        translateX: -100,
+        stagger: 0.2,
+        ease: "power2"
+    });
+
+//our-team
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".team__header",
+        toggleActions: "restart",
+        markers: true
+    }
 })
-.from(".road-map-content__btn__buttons", {
-    duration: 1,
-    opacity: 0,
-    translateX: -100,
+    .from(".team__title", {
+        duration: 1.5,
+        opacity: 0.5,
+        x: -250,
+        stagger: 0.2,
+        ease: "power4"
+    })
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".team__header",
+        toggleActions: "restart",
+        markers: true
+    }
+})
+    .from(".team__description", {
+        duration: 1.5,
+        opacity: 0.5,
+        x: 250,
+        stagger: 0.2,
+        ease: "power4"
+    });
+
+gsap.from(".employee-card._left", {
+    scrollTrigger: {
+        trigger: ".team__header",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 2,
+    opacity: 0.5,
+    x: -1000,
     stagger: 0.2,
-    ease: "power2"
-});
+    ease: "power4"
+})
+
+gsap.from(".employee-card._right", {
+    scrollTrigger: {
+        trigger: ".team__header",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 2,
+    opacity: 0.5,
+    x: 1000,
+    stagger: 0.2,
+    ease: "power4"
+})
+
+gsap.from(".team-content__btn._left", {
+    scrollTrigger: {
+        trigger: ".team-content__buttons",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: -75,
+    opacity: 0.5,
+    ease: "power4"
+})
+
+gsap.from(".team-content__btn._right", {
+    scrollTrigger: {
+        trigger: ".team-content__buttons",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: 75,
+    opacity: 0.5,
+    ease: "power4"
+})
+
+//board-member
+
+gsap.from(".board-member__title", {
+    scrollTrigger: {
+        trigger: ".board-member__header",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: 250,
+    opacity: 0.5,
+    ease: "power4"
+})
+
+
+gsap.from(".board-member__description", {
+    scrollTrigger: {
+        trigger: ".board-member__header",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: -250,
+    opacity: 0.5,
+    ease: "power4"
+})
+
+gsap.from(".board-member-card", {
+    scrollTrigger: {
+        trigger: ".board-member__list",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: -1500,
+    opacity: 0.5,
+    stagger: 0.25,
+    ease: "power4"
+})
+
+gsap.from(".our-partners__header", {
+    scrollTrigger: {
+        trigger: ".our-partners__title",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1.2,
+    opacity: 0,
+    scale: 0,
+    ease: "power4"
+})
+
+
+gsap.from(".our-partners__slider", {
+    scrollTrigger: {
+        trigger: ".our-partners__title",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1.2,
+    opacity: 0.5,
+    scale: 1.33,
+    ease: "power4"
+})
+
+//documents
+
+gsap.from(".documents__header", {
+    scrollTrigger: {
+        trigger: ".documents__header",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: 100,
+    opacity: 0,
+    ease: "power4"
+})
+
+
+gsap.from(".documents__description", {
+    scrollTrigger: {
+        trigger: ".documents__header",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: -100,
+    opacity: 0,
+    ease: "power4"
+})
+
+gsap.from(".documents-list__block._left", {
+    scrollTrigger: {
+        trigger: ".documents-list",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1.5,
+    x: -100,
+    y: 100,
+    scale: 0.8,
+    ease: "circ"
+})
+
+gsap.from(".documents-list__block._right", {
+    scrollTrigger: {
+        trigger: ".documents-list",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1.5,
+    x: 100,
+    y: 100,
+    scale: 0.8,
+    ease: "circ"
+})
+
+gsap.from(".in-press__title", {
+    scrollTrigger: {
+        trigger: ".in-press__title",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1.5,
+    x: -100,
+    scale: 0.8,
+    ease: "circ"
+})
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".in-press__content",
+        toggleActions: "restart",
+        markers: true
+    }
+})
+    .from(".in-press__item._first", {
+        x: -100,
+        opacity: 0,
+        stagger: 0.25,
+        ease: "power1",
+    })
+    .from(".in-press__buttons", {
+        duration: 0.9,
+        y: 100,
+        opacity: 0,
+        ease: "power4"
+    })
+
+gsap.from(".news__title", {
+    scrollTrigger: {
+        trigger: ".news__title",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1.5,
+    x: 100,
+    scale: 0.8,
+    ease: "circ"
+})
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".in-press__content",
+        toggleActions: "restart",
+        markers: true
+    }
+})
+    .from(".news-card._first", {
+        x: 100,
+        opacity: 0,
+        stagger: 0.8,
+        delay: 1.2,
+        ease: "power1",
+    })
+    .from(".news__buttons", {
+        duration: 0.9,
+        y: 100,
+        opacity: 0,
+        ease: "power4"
+    })
+
+//faqs
+
+gsap.from(".faqs__header", {
+    scrollTrigger: {
+        trigger: ".faqs__container",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: 100,
+    opacity: 0,
+    ease: "power4"
+})
+
+
+gsap.from(".faqs__description", {
+    scrollTrigger: {
+        trigger: ".faqs__container",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: -100,
+    opacity: 0,
+    ease: "power4"
+})
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".faqs__animate-rounds",
+        toggleActions: "restart",
+        markers: true
+    }
+})
+    .from(".faqs__animate-rounds", {
+        duration: 1,
+        x: -100,
+        opacity: 0.0,
+        ease: "power1",
+    })
+    .from(".social-media__element._faqs", {
+        x: -100,
+        stagger: 0.3,
+        opacity: 0,
+        ease: "power4"
+    })
+
+gsap.from(".form__title", {
+    scrollTrigger: {
+        trigger: ".faqs__description",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: 50,
+    opacity: 0.5,
+    ease: "power4"
+})
+
+
+gsap.from(".form__description", {
+    scrollTrigger: {
+        trigger: ".faqs__description",
+        toggleActions: "restart",
+        markers: true
+    },
+    duration: 1,
+    x: 100,
+    opacity: 0,
+    ease: "power4"
+})
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".form__description",
+        toggleActions: "restart",
+        markers: true
+    }
+})
+    .from(".form__input-wrapper._name", {
+        duration: 0.4,
+        duration: 1,
+        x: 200,
+        opacity: 0,
+        ease: "power1",
+    })
+    .from(".form__input-wrapper._mail", {
+        duration: 0.4,
+        x: 50,
+        opacity: 0,
+        ease: "power1"
+    })
+    .from(".form__wrap-message", {
+        duration: 0.4,
+        x: 50,
+        opacity: 0,
+        ease: "power1"
+    })
+    .from(".form__primary-button", {
+        duration: 0.4,
+        x: 50,
+        opacity: 0,
+        ease: "power1"
+    })
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".footer",
+        toggleActions: "restart",
+        markers: true
+    }
+})
+    .from(".footer__copyright", {
+        duration: 0.4,
+        opacity: 0,
+        ease: "power1"
+    })
+    .from(".footer__policy", {
+        duration: 0.4,
+        opacity: 0,
+        ease: "power1"
+    })
+    .from(".footer__terms", {
+        duration: 0.4,
+        opacity: 0,
+        ease: "power1"
+    })
