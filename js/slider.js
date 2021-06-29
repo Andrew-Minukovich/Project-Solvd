c1ad086812d3cc2e5a371ae08443e16c1d76a219
Andrew-Minukovich <85671356+Andrew-Minukovich@users.noreply.github.com>
2021-06-29 16:06:11 +0300

    fix: refactor code of slider.js

--- js/slider.js
+++ js/slider.js
@@ -18,7 +18,12 @@ function countdown(dateEnd) {
             dateStart.getUTCHours(),
             dateStart.getUTCMinutes(),
             dateStart.getUTCSeconds());
-        let timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
+        let timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000);
+
+        const daysElement = document.getElementById("days");
+        const hoursElement = document.getElementById("hours");
+        const minuetsElement = document.getElementById("minutes");
+        const secondsElement = document.getElementById("seconds");
 
         if (timeRemaining >= 0) {
             days = parseInt(timeRemaining / 86400);
@@ -29,21 +34,15 @@ function countdown(dateEnd) {
             timeRemaining = (timeRemaining % 60);
             seconds = parseInt(timeRemaining);
 
-
-            document.getElementById("days").innerHTML = parseInt(days, 10);
-            document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
-            document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
-            document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+            daysElement.innerHTML = parseInt(days, 10);
+            hoursElement.innerHTML = ("0" + hours).slice(-2);
+            minuetsElement.innerHTML = ("0" + minutes).slice(-2);
+            secondsElement.innerHTML = ("0" + seconds).slice(-2);
         } else {
             return;
         }
     }
-
-    function display(days, hours, minutes, seconds) { }
 }
-
-
-
 
 countdown('09/22/2022 02:00:00 AM');
 
@@ -53,15 +52,14 @@ new Swiper('.our-partners__slider', {
     centeredSlides: true,
     spaceBetween: 30,
     grabCursor: true,
-
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
     pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
-    }
+    },
 });
 
 new Swiper('.road-map-content__progress-bar', {
@@ -70,27 +68,23 @@ new Swiper('.road-map-content__progress-bar', {
     loop: false,
     direction: 'horizontal',
     watchSlidesVisibility: true,
-
     pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
     },
     navigation: {
         nextEl: '.road-map-content__btn._right',
         prevEl: '.road-map-content__btn._left',
-    }
+    },
 });
 
 new Swiper('.team-content', {
-
     direction: 'horizontal',
     loop: false,
     slidesPerView: 4,
-
     pagination: {
         el: '.swiper-pagination',
     },
-
     navigation: {
         nextEl: '.team-content__btn._right',
         prevEl: '.team-content__btn._left',
@@ -98,15 +92,12 @@ new Swiper('.team-content', {
 });
 
 new Swiper('.news__content', {
-
     direction: 'horizontal',
     loop: false,
     slidesPerView: 1,
-
     pagination: {
         el: '.swiper-pagination',
     },
-
     navigation: {
         nextEl: '.news__btn._right',
         prevEl: '.news__btn._left',
@@ -114,15 +105,12 @@ new Swiper('.news__content', {
 });
 
 new Swiper('.in-press__content', {
-
     direction: 'horizontal',
     loop: false,
     slidesPerView: 1,
-
     pagination: {
         el: '.swiper-pagination',
     },
-
     navigation: {
         nextEl: '.in-press__btn._right',
         prevEl: '.in-press__btn._left',
